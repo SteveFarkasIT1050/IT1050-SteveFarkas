f055229df4c475f6e3b03e702ba79bb634dc373c
Stephen Farkas <S01016610@acad.tri-c.edu>
2020-11-20 13:26:08 -0500

    Lab 5 had us performing various tasks this week identifying the various aspects of a method header, discussing the differences between different methods we may utilize in a project and of course extracting files that required the implementing of multiple methods that tested our ability to successfully input them within a program.

---
+++ Lab5/Lab5/Dog/Dog/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Dog
+    {
+
+        public string Name { get; set; }
+
+
+
+        // Add bark() method
+        public void Bark()
+        {
+            Console.WriteLine("{0} is Barking...", Name);
+        }
+
+
+
+        // Add doTrick() method
+
+        public void doTrick(string trickName)
+        {
+            Console.WriteLine("{0} is so smart!  {0} is doing a(n) {1}", Name, trickName);
+        }
+    } 
+} 
